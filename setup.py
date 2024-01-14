f9c8917f42d370fd2a8e901c5a7f9740c8aab64a
omkardc19 <omkardc18@gmail.com>
2024-01-14 21:36:48 +0530

    setup.py added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-detection",
+    version="0.0.1",
+    author="omkardc19",
+    author_email="omkardc19@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
